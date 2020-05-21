29467200449ee2f47343762050e164530f22743e
rohanpsingh <rohan565singh@gmail.com>
2020-05-21 20:30:21 +0900

    improve logging in train script

--- src/train.py
+++ src/train.py
@@ -1,11 +1,10 @@
 import random
 import argparse
 import os
-import shutil
+import logging
 import numpy as np
 
 import torch
-import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
 
 from dataloader import ObjectKeypointDataset
@@ -55,23 +54,33 @@ def main():
     ap.add_argument("--rate", required=False, default=2.5e-4, type=float)
     args = ap.parse_args()
 
-    print("Training for a total of {} epochs".format(args.epochs))
-    print("at a learning rate of {}".format(args.rate))
-    print("Weights are saved after every {} epochs".format(args.checkpoint))
-    print("Train batch size: {}".format(args.batch_size))
-    print("Valid batch size: {}".format(args.batch_size))
-    print("Total number of keypoints: {}".format(args.num_keypts))
+    #set up logger
+    log_file_path = os.path.join(args.outdir, 'train.log') if args.outdir else 'tmp.log'
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(name)-6s %(levelname)-8s %(message)s',
+                        datefmt='%d/%m/%Y %H:%M:%S',
+                        filename=log_file_path,
+                        filemode='w')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    console.setFormatter(logging.Formatter('%(message)s'))
+    logging.getLogger('').addHandler(console)
+
+    logging.info("Training for a total of %d epochs", args.epochs)
+    logging.info("Weights are saved after every %d epochs", args.checkpoint)
+    logging.info("Total number of keypoints: %d", args.num_keypts)
+    logging.info("Dataset path: %s", args.dataset)
 
     #set manual seeds and defaults
     manual_seed = 0
-    print("Random Seed: ", manual_seed)
+    logging.info("Random Seed: %d", manual_seed)
     random.seed(manual_seed)
     np.random.seed(manual_seed)
     torch.manual_seed(manual_seed)
     torch.cuda.manual_seed(manual_seed)
     torch.cuda.manual_seed_all(manual_seed)
-    cudnn.deterministic=True
-    cudnn.benchmark = False
+    torch.backends.cudnn.deterministic=True
+    torch.backends.cudnn.benchmark = False
     torch.set_default_tensor_type(torch.FloatTensor)
 
     #set network model, loss criterion and optimizer
@@ -79,45 +88,35 @@ def main():
     net = torch.nn.DataParallel(net).cuda()
     criterion = torch.nn.MSELoss(reduction='mean').cuda()
     optimizer = torch.optim.RMSprop(net.parameters(), lr=args.rate)
+    logging.info(repr(optimizer))
 
-    print("Dataset path: {}".format(args.dataset))
     #set train and valid dataloaders
     train_set = ObjectKeypointDataset(os.path.join(args.dataset, "train.txt"), args.num_keypts, 256, 64)
     train_data = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=6)
-    print("train data size is: {} batches of batch size: {}".format(len(train_data), args.batch_size))
+    logging.info("train data size is: %d batches of batch size: %d", len(train_data), args.batch_size)
 
     valid_set = ObjectKeypointDataset(os.path.join(args.dataset, "valid.txt"), args.num_keypts, 256, 64, is_train=False)
     valid_data = DataLoader(valid_set, batch_size=args.batch_size, shuffle=True, num_workers=6)
-    print("valid data size is: {} batches of batch size: {}".format(len(valid_data), args.batch_size))
+    logging.info("valid data size is: %d batches of batch size: %d", len(valid_data), args.batch_size)
 
     if args.outdir:
-        print("Writing to ", args.outdir)
-        if os.path.isdir(args.outdir):
-            shutil.rmtree(args.outdir)
-        log = "args.batch_size: " + repr(args.batch_size) + " args.batch_size: " + repr(args.batch_size) + "\n" + \
-              "train data size is: " + repr(len(train_data)) + " batches of batch size: " + repr(args.batch_size) + "\n" + \
-              "valid data size is: " + repr(len(valid_data)) + " batches of batch size: " + repr(args.batch_size) + "\n" + \
-              "args.num_keypts: " + repr(args.num_keypts) + "\n" + \
-              repr(optimizer)
-        with open(os.path.join(args.outdir,'log'), 'w') as f:
-            f.write("train log... \n")
-            f.write(log + "\n")
+        #create output directory if required
+        if not os.path.isdir(args.outdir):
+            os.makedirs(args.outdir)
+        logging.info("Saving weights to %s", args.outdir)
+    else:
+        logging.info("Output directory not provided. Weights will NOT be saved!!!!")
 
     for epoch in range(args.epochs):
         train_loss = train(net, train_data, criterion, optimizer)
         valid_loss = valid(net, valid_data, criterion)
-        log = "iters: " + repr(epoch) + \
-              " train_loss: " + repr(train_loss.item()) +  \
-              " valid_loss: " + repr(valid_loss.item()) + \
-              " lr: " + repr(optimizer.param_groups[0]['lr'])
-        print(log)
-        print("================")
-        if args.outdir:
-            f = open(os.path.join(args.outdir,'log'), 'a')
-            f.write(log + "\n")
-            f.close()
-            if epoch%args.checkpoint==0:
-                torch.save(net.state_dict(), os.path.join(args.outdir, "trained_" + str(int(epoch)) + ".model"))
+        logging.info("iters: %d train_loss: %f valid_loss: %f lr: %f",
+                     epoch,
+                     train_loss.item(),
+                     valid_loss.item(),
+                     optimizer.param_groups[0]['lr'])
+        if args.outdir and epoch%args.checkpoint==0:
+            torch.save(net.state_dict(), os.path.join(args.outdir, "trained_" + str(int(epoch)) + ".model"))
 
 if __name__=='__main__':
     main()
